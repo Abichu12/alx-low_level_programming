fa703d4aa650781aba957938a5a620cbf4939ca7
Abichu12 <derejeabichu1@gmail.com>
2022-06-04 22:17:54 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#inlude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry
+*
+*Return: Always 0
+*/
+int main(void)
+{
+int k;
+char low;
+for (k = '0'; k <=9; k++)
+putchar(k);
+for (low = 'a';low <= 'f';low++)
+putchar(low);
+putchar('\n');
+return(0);
+}
