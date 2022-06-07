f4a9e1cf16d077df53d40518f0cc73a1254599fa
Abichu12 <derejeabichu1@gmail.com>
2022-06-07 13:52:21 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /**
 * main - Lists all the natural numbers below 1024 (excluded)
 *
@@ -8,7 +8,7 @@ include <stdio.h>
 int main(void)
 {
 int i, sum = 0;
-for (i = 0;i < 1024; i++)
+for (i = 0; i < 1024; i++)
 {
 if ((i % 3) == 0 || (i % 5) == 0)
 sum += i;
