d860b526826f9b9c7900f4ece87b8378122f52e7
Abichu12 <derejeabichu1@gmail.com>
2022-06-09 11:48:20 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* main - label the digit 
+* _isdigit - label the digit 
 * @c: the character to be checked
 *
 * Return: 1 if it is digit, else 0
 */
-int _isdigit(int c);
+int _isdigit(int c)
 {
-if (c >= '0' && c<= '9')
+if (c >= '0' && c <= '9')
 return (1);
 else
 return (0);
