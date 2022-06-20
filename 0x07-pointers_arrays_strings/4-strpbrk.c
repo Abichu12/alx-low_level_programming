af06da207fae8e46ff208f395596c7cbff247d9e
Abichu12 <derejeabichu1@gmail.com>
2022-06-20 11:58:18 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _strpbrk - searches a string
+* @s: char to check
+* @accept: char to look for
+*
+* Return: char
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, k;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (k = 0; accept[k] != '\0'; k++)
+{
+if (s[i] == accept[k])
+return (s + i);
+}
+return (0);
+}
