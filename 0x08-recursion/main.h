c83e89025c63b19d583872a19100fac5d30674ed
Abichu12 <derejeabichu1@gmail.com>
2022-06-23 11:13:15 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-* File: holberton.h
-* Auth: Brennan D Baraban
-* Desc: Header file containing prototypes for all functions
-* used in the 0x07-recursion directory.
-*/
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
