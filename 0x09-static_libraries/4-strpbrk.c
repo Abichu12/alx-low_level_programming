0966b7e3f9d847ebb3d4edbc7dac62dea3b058a9
Abichu12 <derejeabichu1@gmail.com>
2022-06-27 10:35:48 -0700

    create_static_lib.sh

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strpbrk - searches a string
+* @s: char to check
+* @accept: char to look for
+*
+* Return: char
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int index;
+while (*s)
+{
+for (index = 0; accept[index]; index++)
+{
+if (*s == accept[index])
+return (s);
+}
+s++;
+}
+return ('\0');
+}
