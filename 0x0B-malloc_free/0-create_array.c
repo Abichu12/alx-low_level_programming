b6605eb66a1f56581f05b2552f40030bf131d298
Abichu12 <derejeabichu1@gmail.com>
2022-06-27 12:49:13 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,20 +8,14 @@
 */
 char *create_array(unsigned int size, char c)
 {
-char *pointer;
-unsigned int i;
+char *array;
+unsigned int index;
 if (size == 0)
 return (NULL);
-pointer = malloc(sizeof(char) * size);
-/*
-* Allocating a specific nb of bytes on the memory
-* and returning a pointer to the allocated memory
-*/
-if (pointer == NULL)
+array = malloc(sizeof(char) * size);
+if (array == NULL)
 return (NULL);
-else
-{
-for (i = 0; i < size; i++)
-pointer[i] = c;
-return (pointer);
+for (index = 0; index < size; index++)
+array[index] = c;
+return (array);
 }
