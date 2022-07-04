8df27e6c64d0744b101a31d5758b15d18b1ac038
Abichu12 <derejeabichu1@gmail.com>
2022-07-04 11:16:11 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef OBJEKT_LIKE_MACRO
+#define OBJEKT_LIKE_MACRO
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints the name of the file
+* using common predefined macros
+* Return: 0
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
