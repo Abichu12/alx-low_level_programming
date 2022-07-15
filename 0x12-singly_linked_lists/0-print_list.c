8983508c81172bdb85cd43cdf5e011379371dd91
Abichu12 <derejeabichu1@gmail.com>
2022-07-15 12:00:08 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* print_list - print all the elements
+* @h: pointer pointed to the head
+*
+* Return: the number of the elements
+*/
+size_t print_list(const list_t *h)
+{
+size_t length = 0;
+while (h != 0)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+length++;
+h = h->next;
+}
+return (length);
+}
